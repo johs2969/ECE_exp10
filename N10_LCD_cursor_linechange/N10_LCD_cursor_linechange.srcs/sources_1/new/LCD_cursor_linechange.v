df86fc422ebf2dd7229a7ccac39c6a730d2d5169
johs2969 <johs2969@uos.ac.kr>
2022-11-08 13:38:05 +0900

    N10_LCD_cursor_linechange

--- N10_LCD_cursor_linechange/N10_LCD_cursor_linechange.srcs/sources_1/new/LCD_cursor_linechange.v
+++ N10_LCD_cursor_linechange/N10_LCD_cursor_linechange.srcs/sources_1/new/LCD_cursor_linechange.v
@@ -9,31 +9,33 @@ input [1:0] control_btn;
 
 wire [9:0] number_btn_t;
 wire [1:0] control_btn_t;
+wire btn1_t,btn2_t;
 
-oneshot_universal #(.width(12)) uut(.clk(clk), .rst(rst), .btn({number_btn , control_btn}), .btn_trig({number_btn_t , control_btn_t}));
+oneshot_universal #(.width(12)) uut(.clk(clk), .rst(rst), .btn({number_btn , control_btn, btn,~btn }), .btn_trig({number_btn_t , control_btn_t, btn1_t, btn2_t }));
 
 output LCD_E;
 output reg LCD_RS, LCD_RW;
 output reg [7:0] LCD_DATA, LED_OUT;
 
 reg [7:0] cnt;   
-reg [2:0] state;
-reg [7:0] ccnt;
+reg [3:0] state;
+reg [7:0] ccnt= 7'b0000000;
+
 
 parameter DELAY =4'b0000,
           FUNCTION_SET =4'b0001,
           DISP_ONOFF =4'b0010,
           ENTRY_MODE =4'b0011,
-          SET_ADDRESS =4'b0100,
-          SET_LINE_1=4'b0101,
-          SET_LINE_2=4'b0110,
-          DELAY_T =4'b0111,
-          WRITE =4'b1000,
-          CURSOR =4'b1001;
+          SET_ADDRESS =4'b0100,       
+          DELAY_T =4'b0101,
+          WRITE =4'b0110,
+          CURSOR =4'b0111,
+          SET_LINE_1=4'b1000,
+          SET_LINE_2=4'b1001;
 
-          
-reg line = 1'b0;
-          
+reg temp = 1;    
+reg start=0; 
+            
 always @(posedge clk or negedge rst)
 begin
     if(!rst) begin
@@ -69,40 +71,27 @@ begin
             if(cnt == 30) state <= SET_ADDRESS;
         end
         SET_ADDRESS: begin
-           if(cnt == 100) begin
-              if(!line) begin
-              state <= SET_LINE_1;
-              end
-           else begin
-              state <= SET_LINE_2;
-           end
-        end
-             LED_OUT <= 8'b0000_1000;
-        end
-        SET_LINE_1: begin
-        if(cnt >= 30) cnt <= 0;
-          else cnt <= cnt + 1;
-         state <= (btn)? SET_LINE_2:DELAY_T;
-        line <= 1'b0;
-        ccnt <= 7'b0000000;
-        end
-        SET_LINE_2: begin
-          if(cnt >= 30) cnt <= 0;
-        else cnt <= cnt + 1;
-        state <= (!btn)? SET_LINE_1:DELAY_T;
-        line <= 1'b1;
-        ccnt <=7'b1000000;
+            if(cnt >=100) cnt <= 0;
+            else cnt <= cnt+1;
+            LED_OUT <= 8'b0000_1000;
+            if(cnt == 100) state <= DELAY_T;
         end
         DELAY_T :begin
             cnt <= 0;
             LED_OUT <= 8'b0000_0100;
-            state <= |number_btn_t ? WRITE : (|control_btn_t ? CURSOR : DELAY_T);
+            state <= |number_btn_t ? WRITE : (|control_btn_t ? CURSOR : (btn1_t ? SET_LINE_2 : (btn2_t ? SET_LINE_1 : DELAY_T)));  
+//            if(btn1_t == temp) begin
+//                state <= SET_LINE_2;
+//                end
+//            else if(!btn2_t == temp) begin
+//                state <= SET_LINE_1;
+//                end
         end
         WRITE :begin
             if(cnt >= 30) cnt <= 0;
             else cnt <= cnt+1;
             LED_OUT <= 8'b0000_0010;
-            if(cnt == 30) state <= DELAY_T;
+            if(cnt == 30) state <= SET_ADDRESS;
         end
         CURSOR :begin 
             if(cnt >= 30) cnt <= 0;
@@ -110,14 +99,28 @@ begin
             LED_OUT <= 8'b0000_0001;
             if(cnt == 30) state <= SET_ADDRESS;
         end
+         SET_LINE_1 :begin
+            if(cnt >=30) cnt <= 0;
+            else cnt <= cnt+1;
+            LED_OUT <= 8'b0000_1100;
+            if(cnt == 30) state <= DELAY_T;
+         end   
+         SET_LINE_2 :begin
+            if(cnt >=30) cnt <= 0;
+            else cnt <= cnt+1;
+            LED_OUT <= 8'b0000_1010;
+            if(cnt == 30) state <= DELAY_T;
+         end   
      endcase
   end
 end
                               
 always @(posedge clk or negedge rst)
 begin
-    if(!rst)
+    if(!rst) begin
         {LCD_RS, LCD_RW, LCD_DATA} <= 10'b0_0_0000_0001;
+        ccnt <=0;
+        end
     else begin
         case(state)
             FUNCTION_SET :
@@ -127,7 +130,9 @@ begin
             ENTRY_MODE :
                 {LCD_RS, LCD_RW, LCD_DATA} <=10'b0_0_0000_0110;
             SET_ADDRESS :
-                {LCD_RS, LCD_RW, LCD_DATA} <=10'b0_0_0000_0010; // cursor ar home    
+               if(start==0) begin {LCD_RS, LCD_RW, LCD_DATA} <=10'b0_0_0000_0010; // cursor ar home  
+                start <=1;
+                end  
             DELAY_T :
                 {LCD_RS, LCD_RW, LCD_DATA} = 10'b0_0_0000_1111;    
             WRITE : begin
@@ -189,11 +194,15 @@ begin
               else {LCD_RS, LCD_RW, LCD_DATA} <= 10'b0_0_0000_1111;
             end     
           SET_LINE_1: begin
-                 {LCD_RS, LCD_RW, LCD_DATA} <= 10'b0_0_1000_0000; // 0위치로 address를 set
-            end
+                if(~btn) begin {LCD_RS, LCD_RW, LCD_DATA} <= 10'b0_0_1000_0000; // 0위치로 address를 set
+                 ccnt <= 7'b0000000;
+                 end
+              end   
           SET_LINE_2: begin               
-                 {LCD_RS, LCD_RW, LCD_DATA} <= 10'b0_0_1100_0000; // 40위치로 address를 set
-            end          
+                if(btn) begin {LCD_RS, LCD_RW, LCD_DATA} <= 10'b0_0_1100_0000; // 40위치로 address를 set
+                 ccnt <=7'b1000000;
+                 end
+              end          
           endcase
       end
   end
